9eb2242a78a84c3bf665afa12acef590b50a462b
SilverAndrels <andresilva64@outlook.com>
2022-02-06 14:23:34 -0300

    Just a test...

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Mudar em application.properties onde está: senha, pela senha do banco de dados.
+E onde está: root pelo usuário root do banco de dados.
+
+Um teste com spring boot e mysql que "escreve" os nomes e id's dos clientes no banco com os métodos POST, e busca todos os nomes de clientes e os nomes por Id com os métodos GET.
+
+Escrito na IDE Eclipse.
---
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.entities.Cliente;
+import com.example.demo.model.repositorie.ClienteRepository;
+
+@RestController
+@RequestMapping("/nome")
+public class ClienteController {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping
+	public Iterable<Cliente> obterClientes() {
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<Cliente> obterClientePorId(@PathVariable int id) {
+		return clienteRepository.findById(id);
+	}
+	
+	@PostMapping
+	@RequestMapping("/post")
+	public Cliente inserirCliente(@RequestParam String nome) {
+		Cliente cliente = new Cliente(nome);
+		clienteRepository.save(cliente);
+		return cliente;
+	}
+}
