efe69de8ec3711d1d59734c737406f9b989f7ca0
SilverAndrels <andresilva64@outlook.com>
2022-02-08 13:54:47 -0300

    add Delete...

--- src/main/java/com/example/demo/controller/ClienteController.java
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -3,9 +3,11 @@ package com.example.demo.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +38,10 @@ public class ClienteController {
 		Cliente cliente = new Cliente(nome);
 		clienteRepository.save(cliente);
 		return cliente;
+	}
+
+	@DeleteMapping("/del/{id}")
+	public void deleteCliente(@RequestBody Cliente cliente) {
+		clienteRepository.delete(cliente);
 	}
 }
