8a14acdc2ff5fcd500e00186a972bc6bed16f7c7
SilverAndrels <andresilva64@outlook.com>
2022-02-08 19:01:27 -0300

    Delete and Put methods...

--- src/main/java/com/example/demo/controller/ClienteController.java
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -2,18 +2,18 @@ package com.example.demo.controller;
 
 import java.util.Optional;
 
+import com.example.demo.model.entities.Cliente;
+import com.example.demo.model.repositorie.ClienteRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.model.entities.Cliente;
-import com.example.demo.model.repositorie.ClienteRepository;
 
 @RestController
 @RequestMapping("/nome")
@@ -41,7 +41,12 @@ public class ClienteController {
 	}
 
 	@DeleteMapping("/del/{id}")
-	public void deleteCliente(@RequestBody Cliente cliente) {
+	public void deleteCliente(Cliente cliente, Cliente id) {
 		clienteRepository.delete(cliente);
+	}
+
+	@PutMapping("/atualizar/{id}")
+	public Cliente atualizaCliente(Cliente cliente, Cliente id) {
+		return clienteRepository.save(cliente);
 	}
 }
